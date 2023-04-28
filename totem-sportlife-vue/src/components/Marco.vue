bcae7d2947ed4d187fbd6fcdd281875137a1117a
Kevin0003 <c.tranamil.p@gmail.com>
2023-04-28 17:51:46 -0400

    cambio url marco

--- totem-sportlife-vue/src/components/Marco.vue
+++ totem-sportlife-vue/src/components/Marco.vue
@@ -4,8 +4,7 @@
   <!-- la url no se toca. Los cambios se hacen en google drive -->
    <img
     class="fixed w-screen h-screen pointer-events-none"
-    src="https://drive.google.com/uc?id=1Tm7LJdVXnaPZyIdtEfvMfRmpyPQXU_bg"
+    src="https://drive.google.com/uc?id=11pdQMeExjyedKCyUhW7-U9ZLRiC1WoGi"
     alt="marco"
-  
   />
 </template>
